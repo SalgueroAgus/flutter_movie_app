156a9617f38127ace0ff973a311a93761a0dbc09
myusername <salguerocuentas@gmail.com>
2021-02-01 14:19:23 -0300

    test

---
+++ lib/src/pages/pelicula_detalle.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:movies/src/models/actores_model.dart';
+import 'package:movies/src/models/pelicula_model.dart';
+import 'package:movies/src/providers/peliculas_provider.dart';
+
+class PeliculaDetalle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _crearAppbar(pelicula),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10.0,
+              ),
+              _posterTitulo(context, pelicula),
+              _descripcion(pelicula),
+              _descripcion(pelicula),
+              _descripcion(pelicula),
+              _descripcion(pelicula),
+              _descripcion(pelicula),
+              _descripcion(pelicula),
+              _crearCast(pelicula),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _crearAppbar(Pelicula pelicula) {
+    return SliverAppBar(
+      elevation: 2.0,
+      backgroundColor: Colors.indigo,
+      expandedHeight: 300.0,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        background: FadeInImage(
+          image: NetworkImage(pelicula.getBackgroundImg()),
+          placeholder: AssetImage('assets/loading.gif'),
+          fadeInDuration: Duration(milliseconds: 150),
+          fit: BoxFit.cover,
+        ),
+        centerTitle: true,
+        title: Text(
+          pelicula.title,
+          style: TextStyle(color: Colors.white, fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+
+  _posterTitulo(BuildContext context, Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Hero(
+            tag: pelicula.uniqueId,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                image: NetworkImage(pelicula.getPosterImg()),
+                height: 150,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Flexible(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                pelicula.title,
+                style: Theme.of(context).textTheme.headline6,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(pelicula.originalTitle,
+                  style: Theme.of(context).textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis),
+              Row(
+                children: [
+                  Icon(Icons.star_border),
+                  Text(
+                    pelicula.voteAverage.toString(),
+                    style: Theme.of(context).textTheme.subtitle1,
+                  )
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+
+  _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+
+  _crearCast(Pelicula pelicula) {
+    final movieProvider = new PeliculasProvider();
+    return FutureBuilder(
+      future: movieProvider.getCast(pelicula.id.toString()),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return _crearActoresPageView(snapshot.data);
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  _crearActoresPageView(List<Actor> actores) {
+    return SizedBox(
+      height: 200.0,
+      child: PageView.builder(
+        pageSnapping: false,
+        controller: PageController(viewportFraction: 0.3, initialPage: 1),
+        itemCount: actores.length,
+        itemBuilder: (context, i) => _actorCard(actores[i]),
+      ),
+    );
+  }
+
+  _actorCard(Actor actor) {
+    return Container(
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(actor.getFoto()),
+                height: 150.0,
+                fit: BoxFit.cover),
+          ),
+          Text(actor.name, overflow: TextOverflow.ellipsis)
+        ],
+      ),
+    );
+  }
+}
